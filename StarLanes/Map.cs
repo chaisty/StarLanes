d68727c7441a8db6ecb3247fb6d2d2e64240d218
Christopher Haisty <ckhaisty@gmail.com>
2020-04-21 00:02:41 -0400

    Before messing with Bootstrap

--- StarLanes/Map.cs
+++ StarLanes/Map.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace StarLanes
@@ -15,9 +16,138 @@ namespace StarLanes
         public const string StarSector = "*";
         public const string BlackholeSector = "@";
 
-        public Map(int X_Dinmension, int Y_Dimension)
+        public string[,] Sectors = new string[0, 0];
+
+        public Map(int X_Dimension, int Y_Dimension)
+        {
+            Sectors = new string[X_Dimension, Y_Dimension];
+        }
+
+        public string this[int x, int y]
+        {
+            get { return Sectors[x, y]; }
+            set { Sectors[x, y] = value; }
+        }
+
+        public int X_Dimension
+        {
+            get { return Sectors.GetUpperBound(0); }
+        }
+
+        public int Y_Dimension
+        {
+            get { return Sectors.GetUpperBound(1); }
+        }
+
+        public string[] GetNeighbors(int x, int y)
+        {
+            string[] neighbors = new string[4] { " ", " ", " ", " " };  // Initialize to null space (null space is off the board)
+
+            // Get adjacent values
+            if (y > 0)
+                neighbors[0] = Sectors[x, y - 1];
+
+            if (x > 0)
+                neighbors[1] = Sectors[x - 1, y];
+
+            if (x < X_Dimension)
+                neighbors[2] = Sectors[x + 1, y];
+
+            if (y < Y_Dimension)
+                neighbors[3] = Sectors[x, y + 1];
+
+            return neighbors;
+        }
+
+        public List<string> AdjacentCompanies(int x, int y)
+        {
+            string[] neighbors = GetNeighbors(x, y);
+
+            List<string> adjacentCompanies = new List<string>();
+            foreach (string s in neighbors)
+            {
+                // If the sector contains a company and we have not already added this company
+                if (("ABCDEFGHIJKL".Contains(s)) && (!adjacentCompanies.Contains(s)))
+                    adjacentCompanies.Add(s);   // Add the company to the list
+            }
+
+            return adjacentCompanies;
+        }
+
+        public int NumberOfSymbolsOnMap(string symbol)
+        {
+            int size = 0;
+
+            for (int x = Sectors.GetLowerBound(0); x <= Sectors.GetUpperBound(0); x++)
+            {
+                for (int y =Sectors.GetLowerBound(1); y <= Sectors.GetUpperBound(1); y++)
+                {
+                    if (Sectors[x, y] == symbol)
+                        size++;
+                }
+            }
+
+            return size;
+        }
+
+        public int ReplaceSymbolOnMap(string oldValue, string newValue)
+        {
+            int replacements = 0;
+
+            //Update Map
+            for (int x = Sectors.GetLowerBound(0); x <= Sectors.GetUpperBound(0); x++)
+            {
+                for (int y = Sectors.GetLowerBound(1); y <= Sectors.GetUpperBound(1); y++)
+                    if (Sectors[x, y] == oldValue)
+                    {
+                        Sectors[x, y] = newValue;
+                        replacements++;
+                    }
+            }
+
+            return replacements;
+        }
+
+
+
+        //prints the map using text string - for debugging purposes only
+        public string PrintMap(bool showMoves, Dictionary<int, Move> AvailableMoves)
         {
 
+            // Copy the Map so we can overlay the Moves
+            string[,] Map2 = new string[X_Dimension + 1, Y_Dimension + 1];
+            Array.Copy(Sectors, 0, Map2, 0, X_Dimension * Y_Dimension);
+
+            // Add Moves on Map
+            if (showMoves && (AvailableMoves != null))
+            {
+                for (int i = 1; i <= AvailableMoves.Count; i++)
+                {
+                    Map2[AvailableMoves[i].X, AvailableMoves[i].Y] = i.ToString();
+                }
+            }
+
+            // Generate X-Axis Labels
+            string mapString = "&nbsp&nbsp";
+            for (int x = 0; x <= Map2.GetUpperBound(0); x++)
+                mapString += ((char)(x + 65)).ToString() + "&nbsp&nbsp";
+            mapString += "<br />";
+
+            //Generate Rows
+            for (int y = 0; y <= Map2.GetUpperBound(1); y++)
+            {
+                mapString += (y + 1).ToString() + " ";
+                for (int x = 0; x <= Map2.GetUpperBound(0); x++)
+                {
+                    mapString += Map2[x, y] + "&nbsp&nbsp";
+                }
+                mapString += "<br>";
+            }
+
+            //Debug statments to list UBounds on Map Size
+            //mapString += "x=" + Map2.GetUpperBound(0).ToString() + ", Y=" + Map2.GetUpperBound(1).ToString();
+
+            return mapString;
         }
     }
 }
--- StarLanes/Player.cs
+++ StarLanes/Player.cs
@@ -7,13 +7,27 @@ namespace StarLanes
 {
     public class Player
     {
+        public enum PlayerType
+        {
+            Human = 0,
+            Computer1 = 1, // Maximillian - avoids black holes
+            Computer2 = 2, // WOPR - preferse to destroy his opponents
+            Computer3 = 3, // HAL 9001 - goes for the max value move
+        }
+
         public string Name { get; }
         public int Money { get; set; }
+        public PlayerType Type { get; set; }
 
         public Player(string name, int money)
         {
             Name = name;
             Money = money;
+        }
+
+        public bool IsHuman()
+        {
+            return (Type == PlayerType.Human);
         }
     }
 }
