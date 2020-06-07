e57b5e7e53ec178de379795b6d435a82319d974b
Christopher Haisty <ckhaisty@gmail.com>
2020-06-07 00:46:55 -0400

    Adding lots of round-out parts, cleaning up bits

---
+++ StarLanes/MergerResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarLanes
+{
+    public class MergerResult
+    {
+        public int PlayerId { get; private set; }
+        public int NewShares { get; private set; }
+        public int OldShares { get; private set; }
+        public int TotalShares { get; set; }
+        public int Dividend { get; set; }
+
+        public MergerResult(int playerId, int newShares, int oldShares, int totalShares)
+        {
+            PlayerId = playerId;
+            NewShares = newShares;
+            OldShares = oldShares;
+            TotalShares = totalShares;
+        }
+    }
+}
